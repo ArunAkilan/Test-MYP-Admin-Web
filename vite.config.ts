9cb100ec7febd6f7777e5fee88ba4a9ea9eefde2
sumisatech <contact@sumisatech.com>
2025-06-26 07:38:03 +0530

    Update vite.config.ts
    changes by iyyappan

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
    server: {
     host: true,      // needed for Docker (binds to 0.0.0.0)
-    port: 3001,      // desired internal port (changed Port:80 to 3001)
+     port: 80,         // desired internal port (changed Port:80 to 3001)
     strictPort: true,
   
   }
