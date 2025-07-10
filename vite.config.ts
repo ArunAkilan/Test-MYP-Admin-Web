b7cb116a442b7c2bc02c669b155cb756911bd102
sumisatech <contact@sumisatech.com>
2025-07-11 01:50:36 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
    server: {
-     host: true,      // needed for Docker (binds to 0.0.0.0)
-     port: 80
+    host: true,      // needed for Docker (binds to 0.0.0.0)
+     port: 80,         // desired internal port (changed Port:80 to 3001)
+    strictPort: true,
   
-  },
-    build: {
-    chunkSizeWarningLimit: 1500 // in kB
   }
   
   
