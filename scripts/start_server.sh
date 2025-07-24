a319aeba0ee9cc22c7b08a53e911d2b218d18676
sumisatech <contact@sumisatech.com>
2025-07-25 00:08:31 +0530

    fixes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,10 +3,10 @@ echo "=== ApplicationStart Script ==="
 
 IMAGE_TAG=$(cat /home/ubuntu/app/image_tag.txt)
 
-echo "Pulling image: prh_web_2:$IMAGE_TAG"
-docker pull 434648646586.dkr.ecr.us-east-1.amazonaws.com/prh_web_2:$IMAGE_TAG
+echo "Pulling image: simple-docker-service-025017df747b:$IMAGE_TAG"
+docker pull 434648646586.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-025017df747b:$IMAGE_TAG
 
 docker stop myapp || true
 docker rm myapp || true
 
-docker run -d --name myapp -p 80:80 434648646586.dkr.ecr.us-east-1.amazonaws.com/prh_web_2:$IMAGE_TAG
+docker run -d --name myapp -p 80:80 434648646586.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-025017df747b:$IMAGE_TAG
