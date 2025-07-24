6a20a428ab2784535e66c1603b0913d21659b4e3
sumisatech <contact@sumisatech.com>
2025-07-25 01:43:52 +0530

    fixes2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,10 +3,15 @@ echo "=== ApplicationStart Script ==="
 
 IMAGE_TAG=$(cat /home/ubuntu/app/image_tag.txt)
 
-echo "Pulling image: simple-docker-service-025017df747b:$IMAGE_TAG"
-docker pull 434648646586.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-025017df747b:$IMAGE_TAG
+# Generate unique name
+CONTAINER_NAME="myapp-$(date +%s)"  
 
-docker stop myapp || true
-docker rm myapp || true
+echo "Pulling image: simple-docker-service-025017df747b:latest"
+docker pull 434648646586.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-025017df747b:latest
 
-docker run -d --name myapp -p 80:80 434648646586.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-025017df747b:$IMAGE_TAG
+docker stop "$CONTAINER_NAME" || true
+docker rm "$CONTAINER_NAME" || true
+
+docker run -d --name "$CONTAINER_NAME" -p 80:80 434648646586.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service-025017df747b:latest
+
+echo "Started container: $CONTAINER_NAME"
